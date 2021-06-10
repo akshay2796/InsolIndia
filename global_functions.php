620867415cfb8175322c1d146b7b4a3219554ed8
Akshay Kumar <akshay2796@gmail.com>
2021-06-10 09:22:07 +0530

    Fixed ternary in global_functions

--- global_functions.php
+++ global_functions.php
@@ -1112,7 +1112,7 @@ function resizeIMG($TYP,$IMG,$ID,$PATH="")
   
 function selfURL() 
 { 
-    $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : ""; 
+    $s = empty($_SERVER["HTTPS"]) ? '' : (($_SERVER["HTTPS"] == "on") ? "s" : ""); 
     $protocol = strleft(strtolower($_SERVER["SERVER_PROTOCOL"]), "/").$s; 
     $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]); 
     return $protocol."://".$_SERVER['SERVER_NAME'].$port.$_SERVER['REQUEST_URI']; 
